6706848a3c31e0c546224086ac8fca9fe3932246
Krifa Med Aziz <87083945+krifaaziz@users.noreply.github.com>
2025-05-04 01:17:21 +0100

    Implemented theme toggle button and remove button functionality

--- src/js/script.js
+++ src/js/script.js
@@ -9,13 +9,18 @@ let data = [
 ];
 
 const todoListContainer = document.querySelector('.todo-items');
+const todoitem = document.querySelectorAll('.todo-item');
 const itemLeft = document.querySelector('.items_left');
 const checkbox = document.querySelectorAll('.todo-checkbox');
 const newTodoInput = document.querySelector('#new_todo');
 const todoForm = document.querySelector('#todoForm');
-const filterBtns = document.querySelector('.filter_btn');
+const filterBtns = document.querySelectorAll('.filter_btn');
 const btns = document.querySelectorAll('.btn');
 const clearBtn = document.querySelector('#clear_btn');
+const btnToggle = document.querySelector('.toggle_theme');
+const themeIcon = document.querySelector('#theme_icon');
+const todoList = document.querySelector('.todo_list');
+const remove_btns = document.querySelectorAll('.remove_btn');
 
 //Local storage
 const savedData = JSON.parse(localStorage.getItem('TodoData'));
@@ -32,14 +37,23 @@ const renderTodos = function (list) {
   let markup = '';
   list.forEach(todo => {
     markup += `
-            <li class="todo-item" data-id=${todo.id}>
+            <li class="todo-item ${
+              themeIcon.dataset.theme === 'light' ? 'light' : ''
+            }" data-id=${todo.id}>
                 <div class="todo-checkbox ${
                   todo.completed ? 'completed' : ''
                 }"></div>
-                <span class='todoText ${todo.completed ? 'completed' : ''}'>${
-      todo.text
-    }</span
+                <div class="todoText_Conatiner">
+                <span class='todoText ${
+                  todo.completed
+                    ? themeIcon.dataset.theme === 'light'
+                      ? 'completedLight'
+                      : 'completed'
+                    : ''
+                }'>${todo.text}</span
                 >
+                <button class="remove_btn"><i class="fa-solid fa-xmark"></i></button>
+                </div>
             </li>`;
   });
   itemLeft.innerHTML = `${list.length} items left`;
@@ -85,19 +99,21 @@ todoForm.addEventListener('submit', function (e) {
 });
 
 // filter todos
-filterBtns.addEventListener('click', function (e) {
-  const clicked = e.target.closest('.btn');
-  if (!clicked) return;
-  btns.forEach(btn => btn.classList.remove('active'));
-  clicked.classList.add('active');
-  const filterBy = clicked.textContent;
-  const filteredData = data.filter(item => {
-    if (filterBy === 'Active') return item.completed === false;
-    if (filterBy === 'Completed') return item.completed === true;
-    return item;
+filterBtns.forEach(filterbtn => {
+  filterbtn.addEventListener('click', function (e) {
+    const clicked = e.target.closest('.btn');
+    if (!clicked) return;
+    btns.forEach(btn => btn.classList.remove('active'));
+    clicked.classList.add('active');
+    const filterBy = clicked.textContent;
+    const filteredData = data.filter(item => {
+      if (filterBy === 'Active') return item.completed === false;
+      if (filterBy === 'Completed') return item.completed === true;
+      return item;
+    });
+    saveToLocalStorage();
+    renderTodos(filteredData);
   });
-  saveToLocalStorage();
-  renderTodos(filteredData);
 });
 
 // Clear completed
@@ -106,6 +122,50 @@ clearBtn.addEventListener('click', function (e) {
   data = data.filter(item => {
     if (item.completed === false) return item;
   });
+  saveToLocalStorage();
+  renderTodos(data);
+});
+
+// Toggle theme btn
+btnToggle.addEventListener('click', function (e) {
+  e.preventDefault();
+  setTimeout(function () {
+    if (themeIcon.dataset.theme === 'dark') {
+      themeIcon.src = '/src/img/icon-sun.svg';
+      themeIcon.alt = 'sun';
+      themeIcon.dataset.theme = 'light';
+      document.body.classList.remove('dark');
+      document.body.classList.add('light');
+      todoForm.classList.toggle('light');
+      todoList.classList.toggle('light');
+      todoitem.forEach(item => {
+        item.classList.toggle('light');
+      });
+      renderTodos(data);
+    } else {
+      themeIcon.src = '/src/img/icon-moon.svg';
+      themeIcon.alt = 'moon';
+      themeIcon.dataset.theme = 'dark';
+      document.body.classList.add('dark');
+      document.body.classList.remove('light');
+      todoForm.classList.toggle('light');
+      todoList.classList.toggle('light');
+      todoitem.forEach(item => {
+        item.classList.remove('light');
+      });
+      renderTodos(data);
+    }
+  }, 1500);
+});
+
+// Remove Item button
+todoListContainer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const removeBtn = e.target.closest('.remove_btn');
+  if (!removeBtn) return;
+  const btnItem = removeBtn.closest('.todo-item ');
+  if (!btnItem) return;
+  data = data.filter(item => item.id !== parseInt(btnItem.dataset.id));
   saveToLocalStorage();
   renderTodos(data);
 });
